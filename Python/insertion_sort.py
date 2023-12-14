d4ce47c8d80d6e3e9bc0796417cc91c72a5a3b8d
Patrick Martin <patrickwaltermartin@gmail.com>
2023-12-14 10:50:33 -0400

    Adding insertion sort implementation

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -1,2 +1,14 @@
 def insertion_sort(to_sort):
-    pass
+    n = len(to_sort)
+    i = 1
+    while i < n:
+        j = i - 1
+        current = to_sort[i]
+        print(current)
+        while j >= 0 and to_sort[j] > current:
+            to_sort[j+1] = to_sort[j]
+            j-=1
+        to_sort[j+1] = current
+        i+=1
+
+    return to_sort
