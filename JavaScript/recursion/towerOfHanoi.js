72bfb5452face23c7fc897d94f7bc3f822ee15e6
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-19 20:54:02 -0400

    Add javascript and typescript tower of hanoi  examples

---
+++ JavaScript/recursion/towerOfHanoi.js
@@ -0,0 +1,9 @@
+function towerOfHanoi(n, source, helper, destination) {
+    if (n === 0) {
+        return;
+    }
+    towerOfHanoi(n - 1, source, destination, helper);
+    console.log(`Disk ${n} from rod ${source} to rod ${destination}`);
+    towerOfHanoi(n - 1, helper, source, destination);
+}
+towerOfHanoi(3, 'A', 'B', 'C');
