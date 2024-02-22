9bac1602b24ea23c8bb510aca0bc08d8eee4e738
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-21 20:40:59 -0400

    Add start of javascript doubly linked list

---
+++ JavaScript/LinkedLists/doublely_linkedlist.js
@@ -0,0 +1,45 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoubleyLinkedList{
+    constructor(head = null){
+        this.head = head;
+    }
+
+    add(data){
+        const newNode = new Node(data);
+        newNode.next = this.head;
+
+        if(this.head !== null)
+            this.head.prev = newNode
+        
+        this.head = newNode;
+    }
+
+    /**
+    * Print out what the current structure of the list looks like
+    */
+    printList(){
+        let current = this.head;
+        while (current !== null){
+            process.stdout.write(`${current.data} <-> `);
+            current = current.next;
+        }
+        process.stdout.write('NULL\n');
+    }
+}
+
+const list = new DoubleyLinkedList();
+console.log('New Linked List');
+list.printList();
+console.log('Add some values');
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.printList();
