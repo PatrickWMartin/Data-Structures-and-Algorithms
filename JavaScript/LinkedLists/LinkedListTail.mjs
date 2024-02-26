7997916b467a2651e9a8b44e8e0fb95506c524ae
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-26 18:26:43 -0400

    Add a singley linked list with tail in javascript
    Adding this because I will use this for a queue implementation

---
+++ JavaScript/LinkedLists/LinkedListTail.mjs
@@ -0,0 +1,50 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListTail{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    add(data){
+        const newNode = new Node(data);
+        if (this.tail === null){
+            this.head = newNode;
+        } else{
+            this.tail.next = newNode;
+        }
+        this.tail = newNode
+    }
+
+    remove(){
+        this.head = this.head.next;
+    } 
+
+     printList(){
+        let current = this.head;
+        while (current !== null){
+            process.stdout.write(`${current.data} -> `);
+            current = current.next;
+        }
+        process.stdout.write('NULL\n');
+    }
+}
+
+const list = new LinkedListTail();
+console.log('New Linked List');
+list.printList();
+console.log('Add some values');
+list.add(1);
+list.add(2);
+list.add(4);
+list.add(5);
+list.printList();
+console.log('remove some values');
+list.remove();
+list.remove();
+list.printList();
