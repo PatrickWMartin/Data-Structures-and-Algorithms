c8c2900d489e0b5cc333f3dc041a3321085ea497
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-27 08:18:12 -0400

    Add export to linked list tail

---
+++ JavaScript/Queues/Queue.mjs
@@ -0,0 +1,32 @@
+import {LinkedListTail} from '../LinkedLists/LinkedListTail.mjs';
+
+class Queue{
+    constructor(){
+        this.values = new LinkedListTail();
+    }
+
+    enqueue(data){
+        this.values.add(data);
+    }
+    dequeue(){
+        this.values.remove();
+    }
+    printList(){
+        this.values.printList();
+    }
+
+}
+
+const queue = new Queue();
+console.log('New Queue');
+queue.printList();
+console.log('Add some values');
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(4);
+queue.enqueue(5);
+queue.printList();
+console.log('remove some values');
+queue.dequeue();
+queue.dequeue();
+queue.printList();
