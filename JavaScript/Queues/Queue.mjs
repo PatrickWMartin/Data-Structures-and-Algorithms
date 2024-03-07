eb789ab7635a08dbd99c2540827c6068051a6868
Patrick Martin <patrickwaltermartin@gmail.com>
2024-03-07 10:46:44 -0400

    Change queue peek method to return the data

--- JavaScript/Queues/Queue.mjs
+++ JavaScript/Queues/Queue.mjs
@@ -3,13 +3,21 @@ import {LinkedListTail} from '../LinkedLists/LinkedListTail.mjs';
 export class Queue{
     constructor(){
         this.values = new LinkedListTail();
+        this.length = 0;
     }
 
     enqueue(data){
         this.values.add(data);
+        this.length++;
     }
     dequeue(){
-        this.values.remove();
+        if (this.length > 0){
+            this.values.remove();
+            this.length--;
+        }
+    }
+    peek(){
+        return this.values.tail.data;
     }
     printList(){
         this.values.printList();
