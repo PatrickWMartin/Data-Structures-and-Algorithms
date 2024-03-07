8fb6f8c062787f65f2eb445b74484ec16e6d5ebc
Patrick Martin <patrickwaltermartin@gmail.com>
2024-03-07 10:46:19 -0400

    Add insert method for basic binary tree

--- JavaScript/Trees/BasicBinaryTree.mjs
+++ JavaScript/Trees/BasicBinaryTree.mjs
@@ -14,22 +14,45 @@ class BasicBinaryTree{
     }
 
     insert(data){
-        const newNode = BTNode(data);
+        const newNode = new BTNode(data);
 
         if (this.root === null){
             this.root = newNode;
             return;
         }
 
-        const nodeQueue = Queue();
+        const nodeQueue = new Queue();
         nodeQueue.enqueue(this.root);
-        
+        while (nodeQueue.length > 0) {
+            let curr = nodeQueue.peek();
+
+            if (curr.left === null){
+                curr.left = newNode;
+                return;
+            }
+
+            nodeQueue.enqueue(curr.left);
+
+            if (curr.right === null){
+                curr.right = newNode;
+                return;
+            }
+            nodeQueue.enqueue(curr.right);
+            nodeQueue.dequeue();
+        }
 
     }
 
-
-    //insert
     //delete
     //in,pre,post traverse
 
 }
+
+const tree = new BasicBinaryTree();
+
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+tree.insert(4);
+tree.insert(5);
+console.log(tree);
